f1b1228ee649a6a8e3bf52f84885f55da274da95
Muddy Lee <muddy@Muddys-iMac.local>
2018-09-25 18:48:06 -0700

    added comments to better illustrate steps, etc

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+var friendsArray = [
+    {
+        'name' : 'Ken',
+        'friendsURL': 'http://www.unexplainable.net/brainbox/uploads/1/Rabid_Dog_1.jpg',
+        'scores': [
+         5, 6, 3, 6, 7, 2, 5, 3, 1, 2
+        ]
+        
+
+    }
+]
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+//server.js is the "gatekeeper from Thor."  It allows express to see html files from line 26, and allows those files to have access to routes required in lines 40 & 41.
+
+
+//bring in express and body-parser.  Express is a node.js framework.
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var 
+//instantiates an application by calling it's app() method.
 var app = express();
+
+//possible methods:
+//read
+// app.get('/',(req,res)=>{/**/})
+//create
+// app.post('/',(req,res)=>{/**/})
+//update
+// app.put('/',(req,res)=>{/**/})
+//delete
+// app.delete('/',(req,res)=>{/**/})
+// app.patch('/',(req,res)=>{/**/})
 
 //Set the port.  If there isn't an enviornmental port, default to 8080.
 var PORT = process.env.PORT || 8080;
@@ -10,6 +26,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.static('public'))
 
 //sets up the Express app to handle data parsing
+//this allows us to send JSON from html files to 'backend server stuff, like apiRoutes.js'
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
